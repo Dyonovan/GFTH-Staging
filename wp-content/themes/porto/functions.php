ed12410141b50e0b1417dcbd67bee81bb550f9b5
Dyonovan <mcbetacraft@gmail.com>
2015-06-18 19:33:34 -0400

    Removed Desc box from bottom of product page

--- wp-content/themes/porto/functions.php
+++ wp-content/themes/porto/functions.php
@@ -138,6 +138,7 @@ add_action('wp_footer',             'porto_footer_hook', 1);
  */
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
+remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10 );
 
 function porto_css() {
 
